d84e5dcdc657517dfae48f1e358fa5e3ba20639c
Martin Loza <martin.loza.lopez@gmail.com>
2023-05-31 03:50:02 +0000

    version 0.2.2

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,24 @@
+# Canek 0.2.2
+
+## Changes
+
+- Fix bug when using SCTransform.
+([#14](https://github.com/MartinLoza/Canek/issues/14))
+
+- Add BugReports and URL.([#13](https://github.com/MartinLoza/Canek/pull/13))
+
+- Relaxing tests to no consider the names attribute.([#12](https://github.com/MartinLoza/Canek/issues/12))
+
+- Refactor `RunCanek()` Seurat and SingleCellExperiment interfaces to preserve original objects.
+
+- Added integration.name argument to `RunCanek()` to customize the name of the integrated assay.
+
 # Canek 0.2.1
 
 ## Changes
+
 - Fix error in `test-Clustering` unit-test for latest `igraph` version. ([#7](https://github.com/MartinLoza/Canek/issues/7))
 
 # Canek 0.2.0
-* Initial release to CRAN
+
+- Initial release to CRAN
--- R/RunCanek.R
+++ R/RunCanek.R
@@ -5,10 +5,12 @@
 #' @param x object with expression counts or list of matrices.
 #' @param batches for S4 objects the column containing batch information.
 #' @param slot slot used for Seurat objects (default: data).
-#' @param assay assay used for Seurat objects (default: RNA).
+#' @param assay assay used for Seurat objects.
 #' @param features optional vector of features to use for correction.
 #' @param selection.method method used for FindVariableFeatures on Seurat objects when features is NULL.
-#' @param fvf.nfeatures function used to collapse variable features from different batches. Default is intersect.
+#' @param nfeatures  number of features returned by SelectIntegrationFeatures.
+#' @param fvf.nfeatures number of features returned by FindVariableFeatures.
+#' @param integration.name name for the integrated assay.
 #' @param debug whether to store information about correction vector.
 #' @param ... additional arguments passed down to methods.
 #'
@@ -22,42 +24,23 @@ RunCanek <- function(x, ...) {
 
 #' @rdname RunCanek
 #' @export
-RunCanek.Seurat <- function(x, batches = NULL, slot = "data", assay = "RNA", features = NULL, selection.method = "vst", fvf.nfeatures = 2000, debug = FALSE, ...) {
-  x <- Seurat::SplitObject(x, split.by = batches)
-  RunCanek(x, slot = slot, assay = assay, features = features, selection.method = selection.method, fvf.nfeatures = fvf.nfeatures, debug = debug, ...)
-}
+RunCanek.Seurat <- function(x, batches = NULL, slot = "data", assay = NULL, features = NULL, selection.method = "vst", nfeatures = 2000, fvf.nfeatures = 2000, integration.name = "Canek", debug = FALSE, ...) {
 
-#' @rdname RunCanek
-#' @export
-RunCanek.SingleCellExperiment <- function(x, batches = NULL, assay = "counts", debug = FALSE, ...) {
-  batches <- split(colnames(x), x[[batches]])
-  x <- lapply(batches, function(batch) {
-    x[, batch]
-  })
-
-  RunCanek(x, assay = assay, debug = debug, ...)
-}
-
-#' @rdname RunCanek
-#' @export
-RunCanek.list <- function(x, ...) {
-  objtype <- unique(sapply(lapply(x, class), "[", x = 1))
-  if (length(objtype) != 1) stop("Required list of identical object types.")
-  switch(objtype,
-    "Seurat" = RunCanekSeurat(x, ...),
-    "SingleCellExperiment" = RunCanekSingleCellExperiment(x, ...),
-    "matrix" = CorrectBatches(x, ...)
-  )
-
-}
-
-RunCanekSeurat <- function(x, slot = "data", assay = "RNA", features = NULL, selection.method = "vst", nfeatures = 2000, fvf.nfeatures = 2000, debug = FALSE, ...) {
-
-  if (is.null(features)) {
-    features <- Seurat::SelectIntegrationFeatures(x, nfeatures = nfeatures, fvf.nfeatures = fvf.nfeatures, selection.method = selection.method, verbose = FALSE)
+  #if not assay is selected, we used the default one
+  if(is.null(assay)){
+    assay <- Seurat::DefaultAssay(x)
   }
 
-  counts <- lapply(x, function(xx) {
+  Seurat::DefaultAssay(x) <- assay
+  obj <- Seurat::DietSeurat(x, counts = TRUE, data = TRUE, scale.data = FALSE, assays = assay, misc = FALSE)
+  Seurat::VariableFeatures(obj) <- NULL
+  obj <- Seurat::SplitObject(obj, split.by = batches)
+
+  if (is.null(features)) {
+    features <- Seurat::SelectIntegrationFeatures(obj, nfeatures = nfeatures, fvf.nfeatures = fvf.nfeatures, selection.method = selection.method, verbose = FALSE)
+  }
+
+  counts <- lapply(obj, function(xx) {
     Seurat::GetAssayData(xx, slot = slot, assay = assay)[features, ]
   })
 
@@ -70,12 +53,11 @@ RunCanekSeurat <- function(x, slot = "data", assay = "RNA", features = NULL, sel
   }
 
   integrated <- Seurat::CreateAssayObject(counts = counts)
-  x <- Reduce(merge, x)
 
-  x[["Canek"]] <- integrated
-  Seurat::DefaultAssay(x) <- "Canek"
+  x[[integration.name]] <- integrated
+  Seurat::DefaultAssay(x) <- integration.name
 
-  Seurat::VariableFeatures(x, assay = "Canek") <- features
+  Seurat::VariableFeatures(x, assay = integration.name) <- features
 
   if (debug) {
     Seurat::Tool(x) <- info
@@ -84,8 +66,15 @@ RunCanekSeurat <- function(x, slot = "data", assay = "RNA", features = NULL, sel
   Seurat::LogSeuratCommand(x)
 }
 
-RunCanekSingleCellExperiment <- function(x, assay = "logcounts", debug = FALSE, ...) {
-  counts <- lapply(x, SummarizedExperiment::assay, i = assay)
+#' @rdname RunCanek
+#' @export
+RunCanek.SingleCellExperiment <- function(x, batches = NULL, assay = "logcounts", integration.name = "Canek", debug = FALSE, ...) {
+  batches <- split(colnames(x), x[[batches]])
+  obj <- lapply(batches, function(batch) {
+     x[, batch]
+  })
+
+  counts <- lapply(obj, SummarizedExperiment::assay, i = assay)
   counts <- Canek::CorrectBatches(counts, debug = debug, ...)
 
   if (debug) {
@@ -94,8 +83,17 @@ RunCanekSingleCellExperiment <- function(x, assay = "logcounts", debug = FALSE, 
     counts <- counts[["Batches Integrated"]]
   }
 
-  x <- Reduce(SummarizedExperiment::cbind, x)
-  SummarizedExperiment::assays(x, withDimnames = FALSE)[["Canek"]] <- counts
-
+  SummarizedExperiment::assays(x, withDimnames = FALSE)[[integration.name]] <- counts
   x
+}
+
+#' @rdname RunCanek
+#' @export
+RunCanek.list <- function(x, ...) {
+  objtype <- unique(sapply(lapply(x, class), "[", x = 1))
+  if (length(objtype) != 1) stop("Required list of identical object types.")
+  switch(objtype,
+    "matrix" = CorrectBatches(x, ...),
+    stop("When input is a list, it should be a list of matrix objects.")
+  )
 }
